3f1e5281fd7b11c6298896b3aac128ac4428cce6
AaronDeBernardo <142444363+AaronDeBernardo@users.noreply.github.com>
2025-06-14 17:37:16 -0300

    feat: statistics report - fix: some errors
    Co-Authored-By: Joaquin Carlos Fernandez Da Silva <164550681+Joaquipe13@users.noreply.github.com>

--- tp_3/mm1/classes/statistical_counters.py
+++ tp_3/mm1/classes/statistical_counters.py
@@ -1,6 +1,6 @@
 class StatisticalCounters:
     def __init__(self):
-        self.customers_delayed= 0
-        self.total_delay= 0
-        self.area_under_q= 0
-        self.area_under_b= 0
+        self.customers_delayed = 0
+        self.total_delay = 0
+        self.area_under_q = 0
+        self.area_under_b = 0
--- tp_3/mm1/mm1_simulation.py
+++ tp_3/mm1/mm1_simulation.py
@@ -6,7 +6,7 @@ from mm1.classes.system_state import SystemState
 
 
 class MM1Simulation:
-    def __init__(self, arrival_rate, service_rate, sim_time=1000, max_queue=np.inf, random:Generator = default_rng(seed=1234)):
+    def __init__(self, arrival_rate, service_rate, sim_time=1000, max_queue=np.inf, random:Generator = default_rng()):
         self.arrival_rate = arrival_rate
         self.service_rate = service_rate
         self.sim_time = sim_time
@@ -32,6 +32,7 @@ class MM1Simulation:
                 self.handle_arrival()
             else:
                 self.handle_departure()
+            self.system_state.last_event_time = self.clock
         
         self.generate_report()
 
@@ -55,51 +56,45 @@ class MM1Simulation:
 
 
     def handle_arrival(self):
+        self.generate_next_arrival()
         self.update_area_under_b()
-        self.update_area_under_q()
 
         if self.system_state.server_busy == False:
             self.system_state.server_busy = True
             self.statistical_counters.customers_delayed += 1
             self.generate_next_departure()
             
+            
         elif self.system_state.clients_in_queue < self.max_queue:
             self.system_state.clients_in_queue += 1
-
-        else:
-            pass
-        
-        self.system_state.arrival_times.append(self.clock)
-        self.system_state.last_event_time = self.clock
-        self.generate_next_arrival()
+            self.system_state.arrival_times.append(self.clock)
+            self.update_area_under_q()
 
 
     def handle_departure(self):
         self.update_area_under_b()
-        self.update_area_under_q()
-        self.statistical_counters.total_delay += self.calculate_customer_delay()
-
         if self.system_state.clients_in_queue == 0:
-          self.system_state.server_busy = False
-          self.next_departure_time = np.inf
+            self.system_state.server_busy = False
+            self.next_departure_time = np.inf
         else:
-          self.statistical_counters.customers_delayed += 1
-          self.system_state.clients_in_queue -= 1
-          self.generate_next_departure()
+            self.update_area_under_q()
+            self.system_state.clients_in_queue -= 1
+            self.statistical_counters.total_delay += self.calculate_customer_delay()
+            self.statistical_counters.customers_delayed += 1
+            self.generate_next_departure()
 
-        self.system_state.last_event_time = self.clock
 
 
     def generate_next_arrival(self):
-        self.next_arrival_time = self.clock + self.random.exponential(scale=self.arrival_rate)
+        self.next_arrival_time = self.clock + self.random.exponential(scale=1/self.arrival_rate)
 
     def generate_next_departure(self):
-        self.next_departure_time = self.clock + self.random.exponential(scale=self.service_rate)
+        self.next_departure_time = self.clock + self.random.exponential(scale=1/self.service_rate)
 
 
     def calculate_customer_delay(self):
         aux = self.system_state.arrival_times.pop(0)
-        return self.clock-aux
+        return self.clock - aux
 
 
     def update_area_under_b(self):
@@ -114,6 +109,14 @@ class MM1Simulation:
 
 
     def generate_report(self):
-        aux1 = self.system_state
-        aux2 = self.statistical_counters
+        average_customer_in_system = (self.statistical_counters.area_under_q + self.statistical_counters.area_under_b) / self.sim_time
+        average_customer_in_queue = self.statistical_counters.area_under_q / self.sim_time
+        average_time_in_system = self.statistical_counters.total_delay / self.statistical_counters.customers_delayed if self.statistical_counters.customers_delayed > 0 else 0
+        average_time_in_queue = self.statistical_counters.area_under_q / self.statistical_counters.customers_delayed if self.statistical_counters.customers_delayed > 0 else 0
+        server_usage = self.statistical_counters.area_under_b / self.sim_time
+        
+        
+        #Probabilidad de encontrar n clientes en cola.
+        #Probabilidad de denegación de servicio (cola finita de tamaño: 0, 2, 5, 10, 50).
+        
         pass
